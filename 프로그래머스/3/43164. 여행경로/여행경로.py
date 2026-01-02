0ffa1bebf2dd1d8c10d8e332419955f9a0911b7f
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-02 19:44:48 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+def solution(tickets):
+    graph=defaultdict(list)
+    for a,b in tickets:
+        graph[a].append(b)
+    for a in graph:
+        graph[a].sort(reverse=True)
+    stack=["ICN"]
+    route=[]
+    while stack:
+        cur=stack[-1]
+        if graph[cur]:
+            stack.append(graph[cur].pop())
+        else:
+            route.append(stack.pop())
+    return route[::-1]
+        
