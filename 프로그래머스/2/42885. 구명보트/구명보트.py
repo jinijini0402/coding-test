22c80588eb0884d04ef551c3fe04b1b61d21ea0c
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-02 19:45:24 +0900

    [level 2] Title: 구명보트, Time: 9.70 ms, Memory: 9.59 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    people.sort()
+    i,j=0,len(people)-1
+    ans=0
+    while i<=j:
+        if people[i]+people[j]<=limit:
+            i+=1
+            j-=1
+        else:
+            j-=1
+        ans+=1
+    return ans
