4a347f497fbac3fd6910c525667074490260a26b
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-09 21:35:18 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,35 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+d=list(map(int,input().split()))
+op=list(map(int,input().split()))#+,-,*,//
+mx=-10**18
+mn=10**18
+def dfs(idx, cur):
+    global mx,mn
+    if idx==n:
+        mx=max(mx,cur)
+        mn=min(mn,cur)
+        return
+    for i in range(4):
+        if op[i]==0:
+            continue
+        op[i]-=1
+        nxt=d[idx]
+        if i==0:
+            dfs(idx+1,cur+nxt)
+        elif i==1:
+            dfs(idx+1,cur-nxt)
+        elif i==2:
+            dfs(idx+1,cur*nxt)
+        else:
+            if cur<0:
+                div=-(abs(cur)//nxt)
+            else:
+                div=cur//nxt
+            dfs(idx+1,div)
+        op[i]+=1
+dfs(1,d[0])
+print(mx)
+print(mn)
