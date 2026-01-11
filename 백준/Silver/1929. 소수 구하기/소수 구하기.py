6ff39eee1308cfe41338cb6c267013293f61cf2a
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-12 00:27:41 +0900

    [Silver III] Title: 소수 구하기, Time: 288 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,10 @@
 M,N=map(int,input().split())
+check=[True]*(N+1)
+check[0]=check[1]=False
+for i in range (2,int(N**0.5)+1):
+    if check[i]:
+        for j in range (i*i,N+1,i):
+            check[j]=False
 for i in range (M,N+1):
-    if i<2:
-        continue
-    check=True
-    for j in range(2,int(i**0.5)+1):
-        if i%j==0:
-            check=False
-            break
-    if check:
+    if check[i]:
         print(i)
