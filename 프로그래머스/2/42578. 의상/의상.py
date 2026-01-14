4bedc4e9fce27c55691adbda2f9f22d77e009acd
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-14 20:58:24 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,11 @@
 def solution(clothes):
-    kind=[]
-    count=[]
-    for i in clothes:
-        if i[1] not in kind:
-            kind.append(i[1])
-            count.append(1)
+    d=dict()
+    for a,b in clothes:
+        if b not in d:
+            d[b]=1
         else:
-            count[kind.index(i[1])]+=1
-    res=1
-    for i in count:
-        res*=(i+1)
-    return res-1
+            d[b]+=1
+    ans=1
+    for i in d:
+        ans*=(d[i]+1)
+    return ans-1
