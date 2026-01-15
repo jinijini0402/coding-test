8270c1a852b1d041341d702cafa1bbb8434449bc
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-16 01:27:17 +0900

    [Bronze I] Title: 성 지키기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.py
+++ 백준/Bronze/1236. 성 지키기/성 지키기.py
@@ -1,16 +1,16 @@
-r,c=map(int,input().split())
-d=[input() for _ in range (r)]
-rn=0
-cn=0
+R,C=map(int,input().split())
+d=[input() for _ in range (R)]
+Rc=0
+Cc=0
 for i in d:
-    if i.count('X')==0:
-        rn+=1
-for i in range(c):
+    if 'X' not in i:
+        Rc+=1
+for i in range(C):
     check=False
-    for j in range(r):
+    for j in range(R):
         if d[j][i]=='X':
             check=True
             break
     if not check:
-        cn+=1
-print(max(rn,cn))    
+        Cc+=1
+print(max(Rc,Cc))
