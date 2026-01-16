228fdd5b1626f05c26a3e4829a744f512e046243
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-16 16:48:38 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 772 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -1,13 +1,12 @@
 n=int(input())
 d=[list(map(int,input().split())) for _ in range (n)]
-cnt=[0]*n
+res=[0]*n
 for i in range(n):
-    for j in range (n):
+    for j in range(n):
         if i==j:
             continue
-        for k in range (5):
-            if d[i][k]==d[j][k]:
-                cnt[i]+=1
+        for a in range(5):
+            if d[i][a]==d[j][a]:
+                res[i]+=1
                 break
-m=max(cnt)
-print(cnt.index(m)+1)
+print(res.index(max(res))+1)
