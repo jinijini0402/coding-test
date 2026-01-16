5dfac995771a15396815f4233e150bc4efa60ab6
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-16 14:59:26 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,21 +1,20 @@
 def solution(name):
     ans=0
-    for i in name:
-        n=ord(i)-ord('A')
-        if n<=13:
-            ans+=n
+    for ch in name:
+        o=ord(ch)-ord('A')
+        if o<14:
+            ans+=o
         else:
-            ans+=26-n
-    n=len(name)
-    move=n-1
-    for i in range(n):
-        next_i=i+1
-        while next_i<n and name[next_i]=='A':
-            next_i+=1
-        move=min(
-            move,
-            i*2+(n-next_i),
-            (n-next_i)*2+i
-        )
-    ans+=move
-    return ans
+            ans+=26-o
+    l=len(name)
+    mn=l-1
+    for i in range(l-1):
+        if name[i+1]=='A':
+            idx=i+1
+            while idx<l and name[idx]=='A':
+                idx+=1
+            mn=min(
+                mn,
+                i*2+(l-idx),
+                (l-idx)*2+i)
+    return ans+mn
