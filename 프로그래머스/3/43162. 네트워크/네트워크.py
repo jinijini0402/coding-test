7f88c5420d7533470776051345b699b1595eaa67
jinijini0402 <130031341+jinijini0402@users.noreply.github.com>
2026-01-16 14:29:03 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,12 +1,12 @@
 def solution(n, computers):
     visited=[False]*n
     ans=0
-    def dfs(i):
-        visited[i]=True
-        for j in range(n):
-            if computers[i][j]==1 and not visited[j]:
-                dfs(j)
-    for i in range(n):
+    def dfs(idx):
+        for i in range (n):
+            if not visited[i] and computers[idx][i]==1:
+                visited[i]=True
+                dfs(i)
+    for i in range (n):
         if not visited[i]:
             dfs(i)
             ans+=1
